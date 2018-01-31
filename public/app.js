61a9793a7802bfb29fd511e32c95429de6d4477a
derkrisz <krisztian.dergmail.com>
2018-01-31 20:30:28 +0000

    display list of names of beers

--- public/app.js
+++ public/app.js
@@ -1,5 +1,32 @@
+var makeRequest = function(url, callback){
+  var request = new XMLHttpRequest();
+  request.open('GET', url);
+  request.addEventListener('load', callback);
+  request.send();
+}
+
+var requestComplete = function() {
+  if (this.status !== 200)
+  return;
+  var jsonString = this.responseText;
+  beers = JSON.parse(jsonString);
+  populateList(beers);
+}
+
+var populateList = function(beers) {
+  var ul = document.querySelector('#beer-list');
+  beers.forEach(function(beer) {
+    var li = document.createElement('li');
+    li.innerText = beer.name;
+    ul.appendChild(li);
+  })
+}
+
+
+
 var app = function(){
-  console.log('sup!')
+  var url = 'https://api.punkapi.com/v2/beers';
+  makeRequest(url, requestComplete);
 }
 
 document.addEventListener('DOMContentLoaded', app);
